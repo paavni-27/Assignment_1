b8632efdf1b56396b938e17a926d5ceb26338144
Paavni <119272668+paavni-27@users.noreply.github.com>
2023-01-15 09:19:53 +0530

    Add files via upload

---
+++ My.java
@@ -0,0 +1,109 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.util.*;
+public class My {
+    public static void main(String[] args) {
+        /*System.out.println("hello");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter the value of a ");
+        int a = scn.nextInt();
+
+        System.out.println("the value of a is " + a)
+        System.out.println("*******");
+        System.out.println("     *");
+        System.out.println("    *");
+        System.out.println("   *");
+        System.out.println(" *");
+        System.out.println("*******");
+
+        // You are given as input marks of a student.
+        //2. Display an appropriate message based on the following rules:
+        //  	  2.1 for marks above 90, print excellent.
+        //  	  2.2 for marks above 80 and less than equal to 90, print good.
+        //  	  2.3 for marks above 70 and less than equal to 80, print fair.
+        //  	  2.4 for marks above 60 and less than equal to 70, print meets expectations.
+        //  	  2.5 for marks less than equal to 60, print below par.
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("enter students marks : ");
+        int marks = scn.nextInt();
+
+        if(marks>90) {
+            System.out.println("excellent");
+        }
+        else if(marks>80 && marks<=90) {
+            System.out.println("good");
+        } else if(marks>70 && marks<=80) {
+            System.out.println("fair");
+        } else if(marks>60 && marks<=70) {
+            System.out.println("meets expections");
+        } else {
+            System.out.println("below par");
+        }
+
+
+        // to check if a number is a prime number
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int num = scn.nextInt();
+        int i = num / 2;
+
+        for (i = 2; i <= 10; i++) {
+            if (num % i == 0) {
+                System.out.println("it is not a prime number");
+            } else {
+                System.out.println("it is a prime value");
+            }
+            break;
+        }
+
+        // to print prime numbers till n
+        System.out.println("Print any number");
+        Scanner scn= new Scanner(System.in);
+        int n=scn.nextInt(),i=n/2,j;
+        for(i=2;i<=n;i++){
+            if(n%i!=0){
+                int count=0;
+                        count++;
+             if (count=2){
+                 System.out.println(i);
+             }
+            }
+        }
+// print factorial of numbers
+        int n = 5,i;
+        for(i=2;i<n;i++) {
+            int fct=n*i;
+        }
+        System.out.println(n * i);
+
+// write a code to count digits in a number
+        System.out.print("enter any number: ");
+        Scanner scn= new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int digit=0;
+        while(n!=0){
+            n= n/10;
+            digit++;
+        }
+        System.out.println("total digits in the given number is " + digit);
+// Reverse a number
+        System.out.print("enter any number: ");
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+        int rem,rev=0;
+        while(num>0) {
+             rem = num%10;
+             rev= rev*10+rem;
+            num=num/10;
+
+        }
+        System.out.print( "the reverse of the given number is " + rev);
+         */
+
+        //
+
+    }
+}
